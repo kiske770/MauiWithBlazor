c1e211655555a81ed12a470dd0f86b2c5213c40d
Mario Avendano <keppers7keys@hotmail.com>
2022-07-28 17:37:52 -0500

    Add project files.

---
+++ MauiBlazorApp/Services/CharacterService.cs
@@ -0,0 +1,24 @@
+﻿using MauiBlazorApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MauiBlazorApp.Services
+{
+    internal class CharacterService : ICharacterService
+    {
+        const string URL= "https://rickandmortyapi.com/api/character";
+        public async Task<CharactersModel> Get()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(URL);
+            var content =  await response.Content.ReadAsStringAsync();
+            var characterList = JsonSerializer.Deserialize<CharactersModel>(content);
+            return characterList;
+            
+        }
+    }
+}
---
+++ MauiBlazorApp/Services/ICharacterService.cs
@@ -0,0 +1,14 @@
+﻿using MauiBlazorApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBlazorApp.Services
+{
+    internal interface ICharacterService
+    {
+        public Task<CharactersModel> Get();
+    }
+}
